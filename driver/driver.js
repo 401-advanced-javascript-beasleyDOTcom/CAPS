f42d8811eeea9ce8b37a2622d8741028b44b2dde
Beasleydotcom <beasley.com@gmail.com>
2020-09-01 20:37:59 -0700

    I think I got it!

--- driver/driver.js
+++ driver/driver.js
@@ -17,3 +17,52 @@ event - ‘delivered’
 payload - the payload from the data object you just received
 Write that message (as a string) to the CAPS server
 */
+
+require('dotenv').config();
+const net = require('net');
+const socket = new net.Socket();
+const port = process.env.PORT || 3009;
+const host = process.env.HOST || 'localYost';
+
+//connect
+socket.connect({ port: port, host: host}, () =>{
+    console.log('Connected to the server on', host,':', port)
+})
+
+socket.on('data', buffer =>{
+    let raw = buffer.toString();
+    let object = JSON.parse(raw);
+    if(object.event==='pickup'){
+        simulatePickup(object);
+    }
+});
+
+function simulatePickup(object){
+    setTimeout(()=>{
+        let newObject = {
+            event: 'in-transit',
+            payload: object.payload,
+        };
+        console.log('picking up ', newObject.payload.orderID)
+        let stringVersion = JSON.stringify(newObject);
+        socket.write(stringVersion);
+        simulateDelivery(object);
+    }, 1000);
+
+    function simulateDelivery(object){
+        setTimeout(()=>{
+            let message = {
+                event: 'delivered',
+                payload: object.payload,
+            };
+            let stringVersion = JSON.stringify(message);
+            socket.write(stringVersion);
+        }, 3000)
+    }
+}
+/*
+Create a message object with the following keys:
+event - ‘delivered’
+payload - the payload from the data object you just received
+Write that message (as a string) to the CAPS server
+*/
--- vendor/vendor.js
+++ vendor/vendor.js
@@ -14,3 +14,47 @@ If the event is called delivered
 Log “thank you for delivering id” to the console
 Ignore any data that specifies a different event
 */
+
+require('dotenv').config();
+const faker = require('faker');
+const net = require('net');
+const socket = new net.Socket();
+const port = process.env.PORT || 3009;
+const host = process.env.HOST || 'localYost';
+
+
+socket.connect({port: port, host:host}, ()=>{
+    console.log(`Connected to the server on ${host} : ${port}`);
+});
+socket.on('data', buffer =>{
+    let raw = buffer.toString();
+    let object = JSON.parse(raw);
+    checkForDelivered(object);
+});
+function checkForDelivered(object){
+    if(object.event === 'delivered'){
+        console.log('thank you for delivering ', object.payload.orderID)
+    }
+}
+function fakeData(){
+    const storeInfo = {
+    event: 'pickup',
+    time: new Date(),
+    payload: {
+        store: process.env.STORE,
+        orderID: faker.random.uuid(),
+        customer: faker.name.findName(),
+        address: faker.address.streetAddress()
+        }
+    }
+    return storeInfo;  
+}
+
+setInterval(() =>{
+    sendNewOrder()
+}, 5000)
+
+function sendNewOrder(){
+    let newPackage = JSON.stringify(fakeData());
+    socket.write(newPackage);
+}

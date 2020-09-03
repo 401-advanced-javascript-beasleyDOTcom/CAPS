251a7bcbdbe6c2aab0b4e1e14e0c715caf33a9dc
Beasleydotcom <beasley.com@gmail.com>
2020-09-02 22:51:19 -0700

    finally got it working

--- capsHub/caps.js
+++ capsHub/caps.js
@@ -1,4 +1,3 @@
-const { Socket } = require('socket.io-client');
 
 /*
 //Start a socket.io server on a designated port
@@ -22,82 +21,21 @@ caps.on('connection', socket =>{
     socket.on('join', room =>{
         console.log('joined', room);
         socket.join(room);
-    });
+        });
+    socket.on('pickup', payload =>{
+        // let string = JSON.parse(payload);
+        caps.emit('pickup', payload);
+        console.log('EVENT', payload);
+    });        
     socket.on('in-transit', (payload)=>{
         console.log('got to intransit in caps make it in transit baby!', payload)
 
-        caps.to('PigglyWiggly').emit('in-transit', payload);
+        caps.to(process.env.STORE).emit('in-transit', payload);
 
     })
-    socket.on('pickup', payload =>{
-        // let string = JSON.parse(payload);
-        let event = 'pickup';
-        logEvent(event, payload);
-        socket.emit('pickup', payload)
+
+    socket.on('delivered', (payload)=>{
+        caps.to(process.env.STORE).emit('delivered', payload);
     });
-
-
 });
 
-function logEvent(event, string){
-    // console.log(string, 'this is the string')
-    const messageObject = JSON.parse(string);
-    // console.log('this is the message object', messageObject)
-    const payload = messageObject.payload;
-    const time = new Date();
-    console.log('EVENT', {event: event, time, messageObject})
-}
-
-// function deleteSocket(id){
-
-//     return delete socketPool[id];
-// }
-//******************************************************************************************************************************** */
-// const net = require('net');
-// const server = net.createServer();
-// let socketPool = {};
-// const port = process.env.PORT || 3007;
-
-// //this starts the server
-// server.listen(port, () =>{
-//     console.log('Server up and running on port', port);
-// });
-
-// server.on('connection', socket =>{
-//     const id = `Socket-${Math.random()}`;
-//     socketPool[id] = socket;
-//     // console.log(socket, 'has made a connection to the socketPool along with', socketPool);
-    
-
-//     socket.on('data', buffer =>{
-//         const raw = buffer.toString();
-//         onMessageReceived(raw);
-
-//     });
-//     socket.on('close', () =>{
-//         deleteSocket(socket.id);
-//     });
-// });
-
-// function onMessageReceived(string){
-//     logEvent(string);
-//     broadcast(string);
-// }
-// function logEvent(string){
-//     //console.log the big event
-//     const messageObject = JSON.parse(string);
-//     const eventName = messageObject.event;
-//     const payload = messageObject.payload;
-//     const time = new Date();
-//     console.log('EVENT', {event: eventName, time, payload})
-// }
-// function broadcast(string){
-//     for(let key in socketPool){
-//         const socket = socketPool[key];
-//         socket.write(string);
-//     }
-// }
-// function deleteSocket(id){
-
-//     return delete socketPool[id];
-// }
